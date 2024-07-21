2b362e10f29de68e5b8b411269663a31555a80dd
Daniel G Szabo <d.g.szabo-2017@hull.ac.uk>
2024-07-21 22:11:56 +0100

    Add users table to SQL seed file

--- src/lib/sqlSeed.sql
+++ src/lib/sqlSeed.sql
@@ -4,8 +4,13 @@ user_clerk_id TEXT NOT NULL,
 content TEXT NOT NULL
 );
 
-INSERT INTO posts (title, content, likes, category_id) 
-VALUES ('Exploring upcoming trends in technology.', 0),
-('A guide to healthy eating habits.', 0),
-('Basics of investing in the stock market.', 0),
-('Simple DIY projects for your home.', 0);
+CREATE TABLE IF NOT EXISTS users (
+id BIGSERIAL PRIMARY KEY,
+clerk_id TEXT NOT NULL,
+first_name TEXT NOT NULL,
+middle_name TEXT,
+last_name TEXT NOT NULL,
+gender VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+about_me VARCHAR(255)
+);
