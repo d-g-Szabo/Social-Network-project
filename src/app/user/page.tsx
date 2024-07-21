11d332dc54233608224b53fadc968085027c9dd7
Daniel G Szabo <d.g.szabo-2017@hull.ac.uk>
2024-07-21 22:12:58 +0100

    Add user page and redirect logic

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,17 +1,19 @@
-import Image from "next/image";
 import { auth } from "@clerk/nextjs/server";
 import dbConnect from "@/utils/dbConnection";
 import { redirect } from "next/navigation";
 
-export default async function Home() {
+export default async function UserPage() {
   const { userId } = auth();
   const db = dbConnect();
+  // get the user's data from the database
   const userData = (
     await db.query(`SELECT * FROM users WHERE clerk_id = $1`, [userId])
   ).rows[0];
   // if the user has no data, redirect them to the create profile page
   if (!userData) {
     redirect(`/create-profile-page/${userId}`);
+  } else {
+    redirect(`/`);
   }
-  return <main className="">asd</main>;
+  return <></>;
 }
